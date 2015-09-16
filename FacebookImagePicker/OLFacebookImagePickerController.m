09f582dd633d618fcb34bf05c1a490e29224e263
Kostas Karayannis <kkarayannis@gmail.com>
2015-09-16 12:49:34 +0300

    Handle Facebook login to match the old functionality.

--- FacebookImagePicker/OLFacebookImagePickerController.m
+++ FacebookImagePicker/OLFacebookImagePickerController.m
@@ -8,6 +8,8 @@
 
 #import "OLFacebookImagePickerController.h"
 #import "OLAlbumViewController.h"
+#import <FBSDKLoginKit.h>
+#import <FBSDKCoreKit.h>
 
 @interface OLFacebookImagePickerController () <OLAlbumViewControllerDelegate>
 @property (nonatomic, strong) OLAlbumViewController *albumVC;
@@ -18,13 +20,38 @@
 @dynamic delegate;
 
 - (id)init {
-    OLAlbumViewController *albumController = [[OLAlbumViewController alloc] init];
-    if (self = [super initWithRootViewController:albumController]) {
-        self.albumVC = albumController;
-        self.albumVC.delegate = self;
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view.backgroundColor = [UIColor whiteColor];
+    vc.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancelButtonClicked)];
+    if (self = [super initWithRootViewController:vc]) {
+        
     }
     
     return self;
+}
+
+- (void)cancelButtonClicked{
+    [self.delegate facebookImagePicker:self didFinishPickingImages:@[]];
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    if (![FBSDKAccessToken currentAccessToken]){
+        FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
+        [login
+         logInWithReadPermissions: @[@"public_profile"] fromViewController:self
+         handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
+             if (error) {
+                 [self.delegate facebookImagePicker:self didFailWithError:error];
+             } else if (result.isCancelled) {
+                [self.delegate facebookImagePicker:self didFinishPickingImages:@[]];
+             } else {
+                 OLAlbumViewController *albumController = [[OLAlbumViewController alloc] init];
+                 self.albumVC = albumController;
+                 self.albumVC.delegate = self;
+                 self.viewControllers = @[albumController];
+             }
+         }];
+    }
 }
 
 - (void)setSelected:(NSArray *)selected {
