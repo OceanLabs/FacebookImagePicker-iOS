327a1f69097b2c8b431536e88e24b4f76b876d1f
orenk86 <oren@panda-os.com>
2016-02-18 01:44:20 +0200

    fixed crash when photo id is shortener than 11 chars: minimum length of the regex is now 9 and is determined dynamically in a for loop. return empty string if the regex didn't yield any results

--- FacebookImagePicker/OLFacebookImage.m
+++ FacebookImagePicker/OLFacebookImage.m
@@ -98,17 +98,23 @@ static NSString *const kKeyImageHeight = @"co.oceanlabs.FacebookImagePicker.kKey
 - (NSString *)extractPhotoIdFromUrl:(NSURL *)URL
 {
     NSError *error = nil;
-    NSRegularExpression *regex = [NSRegularExpression
-                                  regularExpressionWithPattern: @"_([0-9]{10}[0-9]+)_"
-                                  options: NSRegularExpressionCaseInsensitive
-                                  error: &error];
-    
-    NSString *urlString= [URL absoluteString];
-    NSTextCheckingResult *regexResult = [regex firstMatchInString:urlString options:0 range:NSMakeRange(0, urlString.length)];
-    
-    NSString *subString = [urlString substringWithRange:regexResult.range];
-    
-    return [subString substringWithRange: NSMakeRange(1, subString.length-2)];
+    for (int i = 9; i > 0; i--) {
+        NSString *expressionPattern = [NSString stringWithFormat:@"_([0-9]{%d}[0-9]+)_", i];
+        NSRegularExpression *regex = [NSRegularExpression
+                                      regularExpressionWithPattern:expressionPattern
+                                      options:NSRegularExpressionCaseInsensitive
+                                      error:&error];
+        
+        NSString *urlString= [URL absoluteString];
+        NSTextCheckingResult *regexResult = [regex firstMatchInString:urlString options:0 range:NSMakeRange(0, urlString.length)];
+        
+        NSString *subString = [urlString substringWithRange:regexResult.range];
+        
+        if (subString.length > 2) {
+            return [subString substringWithRange:NSMakeRange(1, subString.length-2)];
+        }
+    }
+    return @"";
 }
 
 #pragma mark - NSCoding protocol methods
